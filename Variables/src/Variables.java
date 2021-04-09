3be21d801c5215bb5fce74f4d40cc121cbb59b81
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-04-09 12:49:53 +0300

    Add files via upload

---
+++ Variables/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myFirstNumner =(10+5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber =myFirstNumner*6;
+        int  MySum = myFirstNumner+mySecondNumber+myThirdNumber;
+        System.out.println("The value of myFirstNumber is : "  + MySum);
+        int myLastOne = 1000-MySum;
+        System.out.println("The value of myFirstNumber is : "  + myLastOne);
+    }
+}
